13b6bccd5080ec84e4bab3bc9c977af6b909c58a
RonanKer29 <kervella.ronan@hotmail.com>
2025-06-19 15:03:13 +0200

    install react router and tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
+import Welcome from "./components/Welcome";
+
 const App = () => {
-  return <div></div>;
+  const [currentStep, setCurrentStep] = useState("welcome"); // 'welcome', 'quiz', 'results'
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedDifficulty, setSelectedDifficulty] = useState("");
+  const [questions, setQuestions] = useState([]);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+
+  return <Welcome />;
 };
 
 export default App;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,13 @@
+const Welcome = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-indigo-100 via-purple-100 to-pink-100 flex items-center justify-center px-4">
+      <div className="bg-white shadow-2xl rounded-2xl border border-gray-300 max-w-xl w-full p-8 space-y-6">
+        <h1 className="text-4xl font-extrabold text-indigo-600 text-center">
+          Welcome to the Quiz
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
